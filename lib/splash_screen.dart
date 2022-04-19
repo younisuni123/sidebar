4d553101d12acc2d3643cf4d876f203387b5c8cb
younisuni123 <younisfromlalazaar@gmail.com>
2022-04-19 15:26:00 +0500

    splash

---
+++ lib/splash_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:sidebar/home.dart';
+import '../utils/routes.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => new SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  startTime() async {
+    var _duration = const Duration(seconds: 3);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushNamed(context, MyRoutes.home);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(bottom: 30.0),
+                  child: new Image.asset(
+                    'assets/img/alnoor-logo.png',
+                    height: 30.0,
+                    fit: BoxFit.scaleDown,
+                  ))
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/img/alnoorlogo.png',
+                width: animation.value * 352,
+                height: animation.value * 140,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,10 @@
+class MyRoutes {
+  static String splash = "/splashScreen";
+  static String bottomBar = "/bottomBar";
+  static String home = "/home";
+  static String login = "/login";
+  static String signUp = "/signup";
+  static String helpSupport = "/helpSupport";
+  static String profile = "/profile";
+  static String temp = "/temp";
+}
